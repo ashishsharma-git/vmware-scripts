91229621cd9749021eecb3e9711967969c049438
ashishsharma-git <40856795+ashishsharma-git@users.noreply.github.com>
2021-07-14 13:56:40 +0530

    Update vSphereCertificateDetails.ps1

--- powershell/vSphereCertificateDetails.ps1
+++ powershell/vSphereCertificateDetails.ps1
@@ -12,28 +12,39 @@ Function Get-VSphereCertificateDetails {
         the certificate details of each ESXi hosts being managed by vCenter Server
     .EXAMPLE
         Get-VSphereCertificateDetails
-#>
+
     if($global:DefaultVIServer.ProductLine -eq "vpx") {
         $vCenterCertMode = (Get-AdvancedSetting -Entity $global:DefaultVIServer -Name vpxd.certmgmt.mode).Value
         Write-Host -ForegroundColor Cyan "`nvCenter $(${global:DefaultVIServer}.Name) Certificate Mode: $vCenterCertMode"
     }
+#>
+# Shell out details of all connected vCenters
+$global:DefaultVIServers | select Name, @{N="Productline"; E={$_.productline}}, @{N="vCenterCertMode"; E={(Get-AdvancedSetting -Entity $_ -Name vpxd.certmgmt.mode).Value}}
 
-    $results = @()
+$results = @()
     $vmhosts = Get-View -ViewType HostSystem -Property Name,ConfigManager.CertificateManager
     foreach ($vmhost in $vmhosts) {
-        $certConfig = (Get-View $vmhost.ConfigManager.CertificateManager).CertificateInfo
-        if($certConfig.Subject -match "vmca@vmware.com") {
-            $certType = "VMCA"
-        } else {
-            $certType = "Custom"
+    # For few hosts the API reported more than 1 certificate so adding another loop to capture them all
+        $certConfigs = (Get-View $vmhost.ConfigManager.CertificateManager).CertificateInfo
+        foreach ($certConfig in $certConfigs) {
+            if($certConfig.Subject -match "vmca@vmware.com") {
+                $certType = "VMCA"
+            } else {
+                $certType = "Custom"
+            }
+            $tmp = [PSCustomObject] @{
+                vCenter = $vmhost.client.ServiceUrl.split("/")[2];
+                VMHost = $vmhost.Name;
+                CertType = $certType;
+                CertCount = $certConfig.count;
+                Status = $certConfig.Status;
+                Expiry = $certConfig.NotAfter;
+                Created = $certConfig.NotBefore;
+                Issuer = $certConfig.Issuer;
+                Subject = $certConfig.subject;
+            }
+            $results+=$tmp
         }
-        $tmp = [PSCustomObject] @{
-            VMHost = $vmhost.Name;
-            CertType = $certType;
-            Status = $certConfig.Status;
-            Expiry = $certConfig.NotAfter;
-        }
-        $results+=$tmp
     }
     $results
 }
